3ff3e133cf41b0b43436fd2067ab538e307d8363
Mbishu2002 <fmbishu@gmail.com>
2023-05-22 04:55:39 +0100

    add CTA and start header

---
+++ portfolio/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react";
+import Header from './components/header/Header'
+import Navbar from './components/navbar/navbar'
+import About from './components/about/about'
+import Experience from './components/Experience/experience'
+import Services from './components/services/services'
+import Portfolio from './components/portfolio/portfolio'
+import Testimonial from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/contact'
+import Footer from './components/footer/footer'
+function App() {
+
+return (
+  <>
+    <Header/>
+    <Navbar/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
+  </>
+  );
+}
+
+export default App;
