6b9d3ee412497827ab312a26dcdbd6671726c5ba
Mbishu2002 <fmbishu@gmail.com>
2023-05-28 20:05:06 +0100

    update contact, about

--- portfolio/src/components/about/about.jsx
+++ portfolio/src/components/about/about.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
+import './about.css'
 
 const About = () => {
   return (
     <section id="about">
+      <h3>About Me</h3>
       <div className="container">
         <div className="about__content">
           <div className="about__text">
             <p>
-            I'm a software developer and machine learning engineer with a passion for innovation and a drive to push the boundaries of technology. With a strong focus on backend development, I have accumulated valuable experience in building scalable and robust systems. Throughout my career, I have had the privilege of working on a range of exciting projects, with Selfbyt being a standout accomplishment.
-
-Selfbyt, the largest project I've been involved in, provided me with the opportunity to showcase my skills and contribute to cutting-edge solutions. Collaborating with diverse teams, I've honed my ability to communicate effectively and work seamlessly towards project goals. I thrive in dynamic environments where I can bring my problem-solving skills to the table and deliver innovative solutions.
-
-Coding is not just a profession for me; it's a true passion. I find immense joy in crafting clean and efficient code that powers functional and intuitive software. From designing robust APIs to optimizing database queries, I revel in the details that make software perform flawlessly.
-
-In addition to coding, I have a flair for writing. I enjoy sharing my knowledge and experiences through technical documentation, articles, and blog posts. Writing allows me to delve deeper into complex concepts, refine my understanding, and contribute to the developer community.
-
-In summary, I'm a dedicated software developer and machine learning engineer who thrives on innovation and embraces challenges. With a strong backend development background, I've contributed to various projects, including Selfbyt. Coding is my passion, and I find joy in crafting elegant solutions. Beyond coding, I also enjoy writing to share my insights and contribute to the broader developer community.
-            </p>
+            I'm a software developer and machine learning engineer passionate about innovation and pushing technology boundaries. With expertise in backend development, I've built scalable systems and contributed to exciting projects like Selfbyt. I excel in dynamic environments, solving problems and delivering innovative solutions. Crafting clean and efficient code brings me joy, and I enjoy sharing knowledge through writing. In summary, I'm a dedicated developer embracing challenges, coding with passion, and contributing to the developer community.</p>
           </div>
         </div>
       </div>
